0d6ab1f61e394584d8fcaad9e3fb7aed7134b9e0
zwowdy <zwowdy@gmail.com>
2014-04-24 16:53:28 +0800

    11

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ $spConfig = array(
 		'host' => 'localhost',  // 数据库地址，一般都可以是localhost
          //'host' => 'www.yimiaofengqiang.com',
 		'login' => 'root', // 数据库用户名
-		'password' => 'N]j]78R>jPKEML7edAC(',  // 数据库密码
-		//'password' => '',
-		'database' => 'xiai' // 数据库的库名称
-		//'database' => 'net37372922' // 数据库的库名称					
+		//'password' => 'N]j]78R>jPKEML7edAC(',  // 数据库密码
+		'password' => '',
+		//'database' => 'xiai' // 数据库的库名称
+		'database' => 'net37372922' // 数据库的库名称					
 	),
 	// smarty配置
 	'view' => array(
@@ -87,7 +87,8 @@ $spConfig = array(
 			//数据库相关 (mysql 连接时, 并且没有设置 UC_DBLINK 时, 需要配置以下变量)
 			'UC_DBHOST' => 'localhost', // UCenter 数据库主机
 			'UC_DBUSER' => 'root', // UCenter 数据库用户名
-			'UC_DBPW' => '', // UCenter 数据库密码
+			'UC_DBPW' => '',
+			//'UC_DBPW' => 'N]j]78R>jPKEML7edAC(', // UCenter 数据库密码
 			'UC_DBNAME' => 'ucenter', // UCenter 数据库名称
 			'UC_DBCHARSET' => 'gbk', // UCenter 数据库字符集
 			'UC_DBTABLEPRE' => 'ucenter.uc_', // UCenter 数据库表前缀，务必注意：最好在表前缀前加上库名
@@ -110,7 +111,7 @@ $spConfig = array(
 		//'file_root_name' => 'articles'
 	),
 	'mode' => 'release',
-	'dispatcher_error' => "import(APP_PATH.'/404.html');exit();",
+	'dispatcher_error' => 'import(APP_PATH."/404.html");exit();',
 	'supe_uid' => '',
 	'SC' => array(
 		'cookiepre' => 'ymfq_', //COOKIE前缀

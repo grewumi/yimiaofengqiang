44e4c4535ce05c950c8f38b51f52ec4d3d1ad9a6
zwowdy <zwowdy@gmail.com>
2014-05-20 22:20:51 +0800

    111

--- include/public-data.php
+++ include/public-data.php
@@ -16,17 +16,17 @@ $website = array(
 	'yuansu'=>array('actType'=>21,'name'=>'爆划算','rank'=>11), // 爆划算
 	'zhekouba'=>array('actType'=>28,'name'=>'折扣吧','rank'=>11), //折扣吧
 	'mmrizhi'=>array('actType'=>27,'name'=>'美美购','rank'=>10), // 美美购
-	
 	'mao'=>array('actType'=>17,'name'=>'特价猫','rank'=>9), // 特价猫
-	'jiejie'=>array('actType'=>25,'name'=>'姐逛街','rank'=>4), // 姐逛街
-	'yimiaofengqiang'=>array('actType'=>20,'name'=>'一秒疯抢','rank'=>6), // 一秒疯抢
 	'vipzxhd'=>array('actType'=>13,'name'=>'vip专享活动','rank'=>8), // vip专享活动
 	'zhuanbao'=>array('actType'=>15,'name'=>'开心赚宝','rank'=>7), //开心赚宝
+	'aitaoba'=>array('actType'=>29,'name'=>'爱淘吧','rank'=>6), //爱淘吧
+	'yimiaofengqiang'=>array('actType'=>20,'name'=>'一秒疯抢','rank'=>5), // 一秒疯抢
 	'mytehui'=>array('actType'=>6,'name'=>'VIP特惠','rank'=>5), // VIP特惠
-	'vipgouyouhui'=>array('actType'=>9,'name'=>'VIP购优惠','rank'=>1), // VIP购优惠
-	'huiyuangou'=>array('actType'=>2,'name'=>'会员购','rank'=>2), // 会员购
+	'jiejie'=>array('actType'=>25,'name'=>'姐逛街','rank'=>4), // 姐逛街
 	'tealife'=>array('actType'=>7,'name'=>'淘牛品','rank'=>3), // 淘牛品
-	'aitaoba'=>array('actType'=>29,'name'=>'爱淘吧','rank'=>5), //爱淘吧
+	'huiyuangou'=>array('actType'=>2,'name'=>'会员购','rank'=>2), // 会员购
+	'vipgouyouhui'=>array('actType'=>9,'name'=>'VIP购优惠','rank'=>1), // VIP购优惠
+	//
 	//'qiang'=>array('actType'=>3,'name'=>'抢牛品','tcounts'=>count($pros->findAll('act_from=3 and '.$where))), // 抢牛品
 	//'10mst'=>array('actType'=>10,'name'=>'秒杀通'), // 秒杀通
 	//'tejiafengqiang'=>array('actType'=>16,'name'=>'特价疯抢'), // 特价疯抢

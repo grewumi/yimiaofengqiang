f4e01819c678b34b6e7ab48f90b4da58ec341b44
Seul <Seul@Seul-PC>
2014-08-09 15:13:56 +0800

    新版本

--- model/m_catmap.php
+++
@@ -1,6 +0,0 @@
-<?php
-class m_catmap extends spModel{
-	var $pk = "id";
-	var $table = "fstk_catmap";
-}
-?>
--- model/m_u.php
+++
@@ -1,6 +0,0 @@
-<?php
-class m_u extends spModel{
-	var $pk = "id";
-	var $table = "fstk_u";
-}
-?>

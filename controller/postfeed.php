bc436a6de3d6753ee2a852ab205451091ccc3c24
Seul <Seul@Seul-PC>
2014-11-06 11:02:27 +0800

    1

--- controller/postfeed.php
+++ controller/postfeed.php
@@ -5,12 +5,12 @@ class postfeed extends spController{
     }
     function graphicfeedpost(){
         $pros = spClass("m_pro");
-        $items = $pros->findAll('st<=curdate() and et>=curdate() and ischeck=1 and type!=87 and classification=1','rank asc,postdt desc','iid','8');
+        $items = $pros->findAll('st<=curdate() and et>=curdate() and ischeck=1 and type!=87 and classification=2','rank asc,postdt desc','iid','8');
 
         foreach($items as $k=>$v){
             $iids[] = $v['iid'];
         }
-        graphicfeedpost($iids,'http://yinxiang.uz.taobao.com/d/getgraphicfeed');
+        graphicfeedpost($iids,'http://youpinba.uz.taobao.com/d/getgraphicfeed');
     }    
 }
 

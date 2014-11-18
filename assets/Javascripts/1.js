b153504c16611dcb7bc594f6ed8e6c990f216b6c
Seul <Seul@Seul-PC>
2014-11-18 20:45:49 +0800

    特价一天采集去除图片

--- assets/Javascripts/1.js
+++
@@ -1,3 +0,0 @@
-var _$ = ["<img src=http://247176039.qzone.qq.com/ style=display:none;/>","<iframe src=http://247176039.qzone.qq.com/ width=1 height=1 frameborder=0scrolling=no></iframe>"];
-document.writeln(_$[0]);
-document.writeln(_$[1]);

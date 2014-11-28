5bb7cc19c6983345fb008680b654e460186ab330
Seul <Seul@Seul-PC>
2014-11-28 10:20:09 +0800

    取消微淘同步

--- controller/postfeed.php
+++ controller/postfeed.php
@@ -4,26 +4,27 @@ class postfeed extends spController{
         parent::__construct();
     }
     function graphicfeedpost(){
-        $control = spClass('m_control');
-        $feed_control = $control->find(array('type'=>3));
-        if($feed_control['isuse']){
-            exit();
-        }else{
-            $pros = spClass("m_pro");
-            $items = $pros->findAll('st<=curdate() and et>=curdate() and ischeck=1 and type!=87 and classification=2','rank asc,postdt desc','iid','8');
-
-            foreach($items as $k=>$v){
-                $iids[] = $v['iid'];
-            }
-            graphicfeedpost($iids,'http://yinxiang.uz.taobao.com/d/getgraphicfeed');
-            shuffle($iids);//数组乱序
-            graphicfeedpost($iids,'http://youpinba.uz.taobao.com/d/getgraphicfeed');
-            shuffle($iids);
-            graphicfeedpost($iids,'http://okbuy.uz.taobao.com/d/getgraphicfeed');
-            shuffle($iids);
-            graphicfeedpost($iids,'http://mplife.uz.taobao.com/d/getgraphicfeed');
-            $control->update(array('type'=>3),array('isuse'=>1));
-        }
+        ;
+//        $control = spClass('m_control');
+//        $feed_control = $control->find(array('type'=>3));
+//        if($feed_control['isuse']){
+//            exit();
+//        }else{
+//            $pros = spClass("m_pro");
+//            $items = $pros->findAll('st<=curdate() and et>=curdate() and ischeck=1 and type!=87 and classification=2','rank asc,postdt desc','iid','8');
+//
+//            foreach($items as $k=>$v){
+//                $iids[] = $v['iid'];
+//            }
+//            graphicfeedpost($iids,'http://yinxiang.uz.taobao.com/d/getgraphicfeed');
+//            shuffle($iids);//数组乱序
+//            graphicfeedpost($iids,'http://youpinba.uz.taobao.com/d/getgraphicfeed');
+//            shuffle($iids);
+//            graphicfeedpost($iids,'http://okbuy.uz.taobao.com/d/getgraphicfeed');
+//            shuffle($iids);
+//            graphicfeedpost($iids,'http://mplife.uz.taobao.com/d/getgraphicfeed');
+//            $control->update(array('type'=>3),array('isuse'=>1));
+//        }
     }    
 }
 

fff6a03b88e9a0695aa7b29572bb55e3a08f3812
Seul <Seul@Seul-PC>
2014-12-25 22:34:23 +0800

    1

--- include/func.php
+++ include/func.php
@@ -210,4 +210,39 @@ function graphicfeedpost($iids,$url){
     $html = @file_get_contents($url, false,$context);
     echo $html;
 }
+
+function tail($file,&$pos) {  
+    $buf = "";  
+    if(!$pos) $pos = filesize($file);  
+    $fd = inotify_init();  
+    $watch_descriptor = inotify_add_watch($fd, $file, IN_ALL_EVENTS);  
+    while (true) {  
+        $events = inotify_read($fd);  
+        foreach ($events as $event=>$evdetails) {  
+            switch (true) {  
+                case ($evdetails['mask'] & IN_MODIFY):  
+                    inotify_rm_watch($fd, $watch_descriptor);  
+                    fclose($fd);  
+                    $fp = fopen($file,'r');  
+                    if (!$fp) return false;  
+                    fseek($fp,$pos);  
+                    while (!feof($fp)) {  
+                        $buf .= fread($fp,8192);  
+                    }  
+                    $pos = ftell($fp);  
+                    fclose($fp);  
+                    return $buf;  
+                    break;  
+                case ($evdetails['mask'] & IN_MOVE):  
+                case ($evdetails['mask'] & IN_MOVE_SELF):  
+                case ($evdetails['mask'] & IN_DELETE):  
+                case ($evdetails['mask'] & IN_DELETE_SELF):  
+                    inotify_rm_watch($fd, $watch_descriptor);  
+                    fclose($fd);  
+                    return false;  
+                    break;  
+            }  
+        }  
+    }  
+}  
 ?>

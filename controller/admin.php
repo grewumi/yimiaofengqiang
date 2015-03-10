0b3a5ffcc95451c38f1008d3391121aab8d6dff2
Seul <Seul@Seul-PC>
2015-03-10 20:14:02 +0800

    1

--- controller/admin.php
+++ controller/admin.php
@@ -505,8 +505,8 @@ class admin extends spController{
                 if($v){
 //                    $this->postDataToUzPhp($item,$k);
                     $url = "http://www.432gou.com/?c=admin&a=postDataToUzPhpForYmfq&item=".urlencode(json_encode($item))."&uz=".$k;
-                    echo $url.'<br />';
-//                    echo file_get_contents($url);
+//                    echo $url.'<br />';
+                    echo file_get_contents($url);
                 }
             }
 //            $this->postDataToUzPhp($item,'admin');

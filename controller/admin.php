d0d9e8a607e4b5dd108da7ff1af8e375112da750
Seul <Seul@Seul-PC>
2015-04-21 19:25:41 +0800

    1

--- controller/admin.php
+++ controller/admin.php
@@ -1071,7 +1071,7 @@ class admin extends spController{
 					'timeout'=>900,
 //                                        'proxy'=>'202.114.144.15:8088',
 //                                        'proxy'=>'101.69.199.99:80',    
-                                        'proxy'=>'218.72.176.89:6668',
+                                        'proxy'=>'220.191.204.142:808',
                                         'request_fulluri' => true
 			));
 //		echo $contents.'<br />';

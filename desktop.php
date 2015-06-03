db4b8f7d385fe5473b4699517e319e4c8e474ffd
Seul <Seul@Seul-PC>
2015-06-04 00:38:35 +0800

    1

--- desktop.php
+++ desktop.php
@@ -1,8 +1,10 @@
 <?php  
     $Shortcut = "[InternetShortcut]  
     URL=http://www.yimiaofengqiang.com/
-    IDList=  
-    [{000214A0-0000-0000-C000-000000000046}]  
+    IDList=[{000214A0-0000-0000-C000-000000000046}]
+    IconFile=http://www.yimiaofengqiang.com/favicon.ico
+    IconIndex=1
+    HotKey=0  
     Prop3=19,2  
     ";  
     Header("Content-type: application/octet-stream");  

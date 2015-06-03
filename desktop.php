98cad9c77d3a797424863de42a4e1bb504e89e2a
Seul <Seul@Seul-PC>
2015-06-04 00:26:18 +0800

    1

---
+++ desktop.php
@@ -0,0 +1,11 @@
+<?php  
+    $Shortcut = "[InternetShortcut]  
+    URL=http://www.yimiaofengqiang.com/
+    IDList=  
+    [{000214A0-0000-0000-C000-000000000046}]  
+    Prop3=19,2  
+    ";  
+    Header("Content-type: application/octet-stream");  
+    header("Content-Disposition: attachment; filename=Ò»Ãë·èÇÀ.url;");  
+    echo $Shortcut;  
+?> 

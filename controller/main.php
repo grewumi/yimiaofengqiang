0e219552f40e37ed56d047f41b8253d73250309a
Seul <Seul@Seul-PC>
2015-06-04 09:10:04 +0800

    1

--- controller/main.php
+++ controller/main.php
@@ -33,8 +33,21 @@ class main extends spController{
 	public function mailindex($mailindex=1){
 		$this->index($mailindex);
 	}
-	
-	public function deal(){
+        public function desktop(){
+            $Shortcut = "[InternetShortcut]  
+            URL=http://www.yimiaofengqiang.com/
+            IDList=[{000214A0-0000-0000-C000-000000000046}]
+            IconFile=http://www.yimiaofengqiang.com/favicon.ico
+            IconIndex=1
+            HotKey=0  
+            Prop3=19,2  
+            ";  
+            Header("Content-type: application/octet-stream");  
+            header("Content-Disposition: attachment; filename=Ò»Ãë·èÇÀ.url;");  
+            echo $Shortcut;
+        } 
+
+        public function deal(){
 		$id = $this->spArgs('id');
 		$pros = spClass("m_pro");
 		$pro = $pros->find(array('id'=>$id));

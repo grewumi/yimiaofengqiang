470fff42c68401fe0f2d4d4e2585c78a9994b40b
Seul <Seul@Seul-PC>
2015-06-13 11:03:12 +0800

    1

--- include/func.php
+++ include/func.php
@@ -56,7 +56,7 @@ function get_contents($url){
 //                $loginpassw = 'qq576342340:576342340';
 //                $proxy_ip = '107.150.60.66';
 //                $proxy_port = '62001';
-                $proxy = 'http://183.61.240.81:80';
+                $proxy = 'http://120.198.243.79:80';
 		$ch = curl_init();
 		curl_setopt($ch,CURLOPT_URL,$url);
                 curl_setopt($ch,CURLOPT_PROXY,$proxy);

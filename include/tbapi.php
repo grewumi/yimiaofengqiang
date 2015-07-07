f00f8bad9de448d30c8078f4812ec91d828f2a0a
Seul <Seul@Seul-PC>
2015-07-07 10:39:55 +0800

    1

--- include/tbapi.php
+++ include/tbapi.php
@@ -89,9 +89,9 @@ function getItem($num_iid,$mode='taoke')
 		$req = new TbkItemInfoGetRequest;
                 $req->setFields("num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url");
                 $req->setNumIids($num_iid);
-		$resp = $c->execute($req);
+                $resp = $c->execute($req);
 		var_dump($resp);
-		$resp = object_to_array($resp->n_tbk_item);
+//		$resp = object_to_array($resp->n_tbk_item);
 //		var_dump($resp);
 	}
 	elseif($mode == 'taoke'){

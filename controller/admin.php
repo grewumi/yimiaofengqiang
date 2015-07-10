68a060825f5dcf2627ee94e976140ede62a81d3b
Seul <Seul@Seul-PC>
2015-07-10 11:37:37 +0800

    1

--- controller/admin.php
+++ controller/admin.php
@@ -1017,8 +1017,8 @@ class admin extends spController{
             ini_set('pcre.recursion_limit', 99999); // 资源开大就行
             // end - 采集开春哥
             import("tbapi.php");
-            var_dump($this->spArgs("page"));
-            foreach($this->spArgs("page") as $k => $v){
+//            var_dump($this->spArgs("page"));
+            foreach(get_TbkItems($this->spArgs("page")) as $k => $v){
                 $tkitem[] = array('iid'=>$v[3]);
             }
             $tkitems['all'] = $tkitem;

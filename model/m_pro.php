6e3e8ffd7138a071f80596aba6965533e5a18503
Seul <Seul@Seul-PC>
2015-07-15 11:57:04 +0800

    1

--- model/m_pro.php
+++ model/m_pro.php
@@ -2,5 +2,8 @@
 class m_pro extends spModel{
 	var $pk = "id";
 	var $table = "fstk_pro";
+        function getmypage($conditions,$order, $page, $pageSize){
+           return $this->spPager($page, $pageSize)->findAll($conditions,$order);
+        }
 }
 ?>

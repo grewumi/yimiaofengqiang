d5933acd1f36735501ba46707125e4be1f0c910a
Seul <Seul@Seul-PC>
2016-01-09 18:22:36 +0800

    1

--- controller/admin.php
+++ controller/admin.php
@@ -1460,7 +1460,7 @@ class admin extends spController{
                 $catmaps = spClass("m_catmap");
                 import('tbapi.php');
 		$where = 'st<=curdate() and et>=curdate() and ischeck=1 and cat=42';
-		$items = $pros->findAll($where,'','','100');
+		$items = $pros->findAll($where);
 		foreach($items as $k=>$v){
                     $pcid = getPcidNew($v['iid']);
                     // 查询fstk_catmap对应类目
